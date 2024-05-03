861684b242a94bb9fd9d7cee4575083b93d71db2
Vaishnavi Tiwari <vaishnavi.t@ipexcel.com>
2024-05-03 16:19:02 +0530

    several changes

---
+++ main.py
@@ -0,0 +1,3 @@
+from textSummarizer.logging import logger
+
+logger.info("welocome to our custom logging")
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+REPO_NAME = "TextSumm" 
+AUTHOR_USER_NAME = "V415hT313"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "vashu2506rkt@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author_email=AUTHOR_EMAIL,
+    description="A python package"
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/V415hT313/TextSumm",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packeges(where="src")
+)
